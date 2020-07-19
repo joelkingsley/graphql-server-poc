3af6665d292e82299b298775fd3d7555a4702db7
joelkingsleyr <jkingsley@nvenergy.com>
2020-07-19 16:07:31 +0530

    Added GraphQL server dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typeDefs: ['./**/*.graphql'],
+      definitions: { path: join(process.cwd(), 'src/graphql.ts') }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
